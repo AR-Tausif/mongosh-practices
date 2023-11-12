31a70e45f00e53c6c3b1781c18479d9140588a83
Tausif Ahmed <tausif.ritu1@gmail.com>
2023-11-13 00:37:30 +0600

    Retrieve a list of unique friend names for individuals who have at least one friend, and include only the friends with names starting with the letter

--- mongodbAggregiate.ts
+++ mongodbAggregiate.ts
@@ -64,7 +64,36 @@ db.getCollection("massive-data").aggregate([
 
 // Question 4:
 //- Retrieve a list of unique friend names for individuals who have at least one friend, and include only the friends with names starting with the letter "W."
-
+// db.getCollection("massive-data").aggregate([
+//   {
+//     $unwind: "$friends"
+//   },
+//   {
+//     $match: {
+//       "friends.name": /^W/
+//     }
+//   },
+//   {
+//     $group: {
+//       _id: "$_id",
+//       friends: { $addToSet: "$friends.name" }
+//     }
+//   }
+// ])
+db.getCollection("massive-data").aggregate([
+    // stage-1
+    {
+        $unwind: "$friends"
+    },
+    // stage-2
+    {
+        $match: {"friends.name": /^W/}
+    },
+    // stage-3
+    {
+        $project: {"friends.name": 1}
+    }
+    ])
 
 //Question 5:
 //- Use $facet to separate individuals into two facets based on their age: those below 30 and those above 30. Then, within each facet, bucket the individuals into age ranges (e.g., 20-25, 26-30, etc.) and sort them by name within each bucket.
