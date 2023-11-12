3b6c0e0215f5a13f786fc170515767dfed45192a
Tausif Ahmed <tausif.ritu1@gmail.com>
2023-11-12 21:40:37 +0600

    Find the average age of individuals for each favorite fruit, then sort the results in descending order of average age.

--- mongodbAggregiate.ts
+++ mongodbAggregiate.ts
@@ -18,6 +18,7 @@ db.getCollection("massive-data").aggregate([
 
 //Question 2:
 //- Retrieve the names and email addresses of individuals who are active (`isActive: true`) and have a favorite fruit of "banana."
+
 db.getCollection("massive-data").aggregate([
     {
       $match: {isActive: true, favoriteFruit: "banana"}  
@@ -40,6 +41,26 @@ db.getCollection("massive-data").aggregate([
     ])
 //Question 3:
 //- Find the average age of individuals for each favorite fruit, then sort the results in descending order of average age.
+
+db.getCollection("massive-data").aggregate([
+    // Stage-1
+    {
+        $group: { 
+            _id: "$favoriteFruit", 
+            count: { 
+                $sum: 1
+            }, 
+            avgQuantity: { 
+                $avg: "$age" 
+                
+            } 
+        }
+    },
+    // Stage-2
+    {
+        $sort: {avgQuantity: 1}
+    }
+])
 
 // Question 4:
 //- Retrieve a list of unique friend names for individuals who have at least one friend, and include only the friends with names starting with the letter "W."
