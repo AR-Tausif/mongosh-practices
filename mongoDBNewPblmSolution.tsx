f36eb164d1acacf91165568912ceed01685d7913
Tausif Ahmed <tausif.ritu1@gmail.com>
2023-11-14 07:17:36 +0600

    Find the average order quantity and price for each product category. Display the category, average quantity, and average price.

--- mongoDBNewPblmSolution.tsx
+++ mongoDBNewPblmSolution.tsx
@@ -37,7 +37,26 @@ db.products.aggregate([
 ])
 
 // Find the average order quantity and price for each product category. Display the category, average quantity, and average price. 
- 
+ db.orders.aggregate([
+    {
+        $unwind: "$products"
+    },
+    {
+        $lookup: {
+               from: "products",
+               localField: "products.product_id",
+               foreignField: "_id",
+               as: "productDetails"
+             }
+    },
+    {
+        $unwind: "$productDetails"
+    }, 
+    {
+        $group: { _id: "$productDetails.category", count: {$sum: 1}, avgQty: {$avg: "$products.quantity"}, avgPrice: {$avg: "$productDetails.price"} }
+    }
+    ])
+
 // Calculate the total quantity and average price of products in each order. Display the order ID, total quantity, and average price.
 
 // For the "users" collection, suggest an indexing strategy to optimize queries related to user authentication, considering fields like email. 
